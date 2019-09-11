3e5bda160653cb6333f6515b6b59358397c305fe
Grant Jackson <gej1515@gmail.com>
2019-09-10 21:03:35 -0500

    added the events section on the artist page

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,34 @@
+
+function testEvents() {
+    let testUrlEvents = 'https://rest.bandsintown.com/artists/cold%20war%20kids/events?app_id=1e140eabdce95250b1ad6075934a113d'
+    $.ajax({
+        url: testUrlEvents,
+        method: 'GET',
+    })
+        .then(function(response){
+            console.log(response) 
+                for (i = 0; i < response.length; i++){
+                    let city = response[i].venue.city
+                    console.log (city)
+                    let date = moment(response[i].datetime).format('MMM Do');
+                    console.log(date)
+                    let event = $('<li>').text(date + " " + city) 
+                    event.attr({
+                        src: response[i].offers[0].url,
+
+                    })   
+                    $('#events').append(event) 
+                }
+            })
+    }
+testEvents()
+
+$(document).on('click', 'li', move)
+
+function move() {
+    console.log($(this).attr('src'))
+    window.open($(this).attr('src'))
+
+
+}
+
