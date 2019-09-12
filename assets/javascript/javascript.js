539fd529d46da760a9a0ecad81beb889ef8709e7
Grant Jackson <gej1515@gmail.com>
2019-09-12 18:38:53 -0500

    added changes to the search tab

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,19 @@
 
+$(document).ready(function(){
+
+    $("#artistSearch").on("click", function(event) {
+    event.preventDefault();
+    location.href = 'artistPage.html'
+    let newArtist = $('#artistName').val().trim()
+    console.log(newArtist)
+})
+//     $("#artistSearch").click(function(event){
+//       event.preventDefault();
+//       let newArtist = $('#artistSearch').val().trim()
+//     console.log(newArtist)
+// })
+   
+
 function testEvents() {
     let testUrlEvents = 'https://rest.bandsintown.com/artists/cold%20war%20kids/events?app_id=1e140eabdce95250b1ad6075934a113d'
     $.ajax({
@@ -6,12 +21,12 @@ function testEvents() {
         method: 'GET',
     })
         .then(function(response){
-            console.log(response) 
+            // console.log(response) 
                 for (i = 0; i < response.length; i++){
                     let city = response[i].venue.city
-                    console.log (city)
+                    // console.log (city)
                     let date = moment(response[i].datetime).format('MMM Do');
-                    console.log(date)
+                    // console.log(date)
                     let event = $('<li>').text(date + " " + city) 
                     event.attr({
                         src: response[i].offers[0].url,
@@ -28,7 +43,6 @@ $(document).on('click', 'li', move)
 function move() {
     console.log($(this).attr('src'))
     window.open($(this).attr('src'))
-
-
 }
 
+});
